88c441fae005bb6ae4e274d49061e8ef40132d4d
jeremykhoo <jeremykhoois@pm.me>
2023-12-13 23:02:27 +0800

    added shadows

--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@
 #include "light.h"
 #include "shape/shape.h"
 
+/**
+ * @brief calcualtion of reflected ray using Snell's law
+ * @param const Vec3f I: incident ray
+ * @param const Vec3f N: normal of incidence
+ */
 Vec3f reflect(const Vec3f &I, const Vec3f &N) {
     return I - N*2.f*(I*N);
 }
 /**
- * @brief for each sphere, check if intersect. 
+ * @brief for each sphere, check if intersect, render the material of the closer sphere
  * @param const Vec3f orig: ray origin
  * @param const Vec3f dir: direction of ray
  * @param const Sphere sphere: sphere
@@ -47,6 +52,13 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
     float diffuse_light_intensity = 0, specular_light_intensity = 0;
     for(size_t i=0; i< lights.size(); i++) {
         Vec3f light_dir = (lights[i].position - hit).normalize();
+        float light_distance = (lights[i].position - hit).norm();
+
+        Vec3f shadow_orig = light_dir*N < 0 ? hit - N*1e-3 : hit + N*1e-3; // checking if the point lies in the shadow of the lights[i]
+        Vec3f shadow_pt, shadow_N;
+        Material tmpmaterial;
+        if (view_ray_intersect(shadow_orig, light_dir, spheres, shadow_pt, shadow_N, tmpmaterial) && (shadow_pt-shadow_orig).norm() < light_distance)
+            continue;
         diffuse_light_intensity += lights[i].intensity * std::max(0.f,light_dir*N);
         specular_light_intensity += powf(std::max(0.f, reflect(light_dir, N)*dir), material.specular_exponent)*lights[i].intensity;
     }
@@ -89,18 +101,21 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
 }
 
 int main() {
-    Material blue(Vec2f(0.3,  0.6),Vec3f(0.2, 0.55, 0.91), 50);
-    Material red(Vec2f(0,  1),Vec3f(0.3, 0.1, 0.1),50);
+
+    Material blue(Vec2f(0.9,  0.9),Vec3f(0.2, 0.55, 0.91), 50);
+    Material red(Vec2f(0.3, 0.6),Vec3f(0.3, 0.1, 0.1),50);
     Material green(Vec2f(0.3,  0.6),Vec3f(0.35, .8, 0.42),10);
-    Material yellow(Vec2f(0.3,  0.6),Vec3f(0.76, 0.78, 0.31),10);
+    Material yellow(Vec2f(0., 0.6),Vec3f(0.76, 0.68, 0.2),10);
 
     std::vector<Sphere> spheres;
-    spheres.push_back(Sphere(Vec3f(-3,    0,   -16), 2, yellow));
+    spheres.push_back(Sphere(Vec3f(-3,    0,   -16), 2, blue));
     spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red));
-    spheres.push_back(Sphere(Vec3f( 0, 0, -18), 3, green));
-    spheres.push_back(Sphere(Vec3f( 3.5, 2.5, -15), 3.2, blue));
+    spheres.push_back(Sphere(Vec3f( 1.5, -0.5, -18), 3, yellow));
+    spheres.push_back(Sphere(Vec3f( 7,    5,   -18), 4, green));
     std::vector<Light>  lights;
-    lights.push_back(Light(Vec3f(-20, 20, 20), 1.2));
+    lights.push_back(Light(Vec3f(-20, 20,  20), 1.5));
+    lights.push_back(Light(Vec3f( 30, 50, -25), 1.8));
+    lights.push_back(Light(Vec3f( 30, 20,  30), 1.7));
     render(spheres, lights);
     return 0;
 }
